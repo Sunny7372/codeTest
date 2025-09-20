714734263536834715d15c73ff556053c6c0ec3d
Sunny7372 <sunnymishra7372@gmail.com>
2025-09-20 17:38:24 +0530

    new changes done from suny kumar

--- src/main/java/com/filedownload/Coding.java
+++ src/main/java/com/filedownload/Coding.java
@@ -3,6 +3,7 @@ package com.filedownload;
 public class Coding {
     public static void main(String[]args){
         int[]arr={1,2,3,4,6,7, 8,9,10};
+        System.out.println("something  better");
         /*for(int i=0;i<arr.length-1;i++){
             if(arr[i+1]-arr[i]!=1){
                 int missingNumber=arr[i]+1;
